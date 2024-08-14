490bec8f30aa9d104e3898d707a7fe4d99d94a91
Michael-Elrod-dev <michaelelrod.dev@gmail.com>
2024-08-14 19:04:43 -0400

    Portfolio section moved

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,8 @@ export default function Home() {
         </section>
 
         <About />
-        <Skills />
         <Portfolio />
+        <Skills />
         <Contact />
       </main>
     </Layout>
