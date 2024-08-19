4e0a11bcf56ba2782902d60ff58158e7a08b1459
Michael-Elrod-dev <michaelelrod.dev@gmail.com>
2024-08-19 15:51:28 -0400

    item order change

--- app/page.tsx
+++ app/page.tsx
@@ -36,8 +36,8 @@ export default function Home() {
         </section>
 
         <About />
-        <Portfolio />
         <Skills />
+        <Portfolio />
         <Contact />
       </main>
     </Layout>
